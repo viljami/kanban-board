1e8014707c606995ce4f61d2fe93b0306830c213
Viljami Peltola <viljami.peltola@iki.fi>
2016-12-02 08:28:06 +0200

    More functional style server reducer.

--- server/src/reducer.js
+++ server/src/reducer.js
@@ -7,6 +7,7 @@ const states = ['todo', 'inProgress', 'done'];
 const initialState = [];
 
 let id;
+
 for (id = 0; id < 10; ++id){
   initialState.push({
     id: id,
@@ -15,20 +16,32 @@ for (id = 0; id < 10; ++id){
   });
 }
 
+function task (state = {}, action = {}) {
+  switch (action.type) {
+    case actionTypes.CREATE_TASK:
+      return {
+        id: ++id,
+        text: action.text || 'New Task',
+        state: action.state || 'todo'
+      };
+    case actionTypes.UPDATE_TASK:
+      if (state.id !== action.id) return state;
+      if (! action.text && ! action.state) return state;
+      const o = {};
+      if (action.text) o.text = action.text;
+      if (action.state) o.state = action.state;
+      return Object.assign({}, state, o);
+    default:
+      return state;
+  }
+}
+
 module.exports = function reducer(state = initialState, action = {}) {
   switch (action.type) {
     case actionTypes.CREATE_TASK:
-      return [
-       ...state, {
-        id: ++id,
-        text: action.text,
-        state: action.state
-       }];
+      return [...state, task(undefined, action)];
     case actionTypes.UPDATE_TASK:
-      const task = state.find(o => o.id === action.id);
-      if (action.text) task.text = action.text;
-      if (action.state) task.state = action.state;
-      return state;
+      return state.map(o => task(o, action));
     default:
       return state;
   }
